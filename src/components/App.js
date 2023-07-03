24f6880e2bb8baab66a6cb936a1a85bfdfeed572
coolsachin2002@gmail.com <coolsachin2002@gmail.com>
2023-07-03 17:17:48 +0530

    button component and display component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
 import Display from "./Display";
 import Buttons from "./Buttons";
+import { useState } from "react";
+// let result = Function("return " + state)();
 
 function App() {
+
+  const [state, setState] = useState("");
+
+  function handle(e){
+   switch (e.target.value) {
+    case "=":
+    let result = Function("return " + state)();
+     setState(result);
+      break;
+    case "C":
+     setState("");
+      break;
+    case "+/-":
+     let result1 = Function(state)();
+     result1=result1*-1;
+     setState(result1);
+    default:
+     setState(state+e.target.value);
+      break;
+   }
+  }
     
   return (
-    <>
-       <Display/>
-   <Buttons/>
-    </>
-
+    <div className="block">
+         <Display value={state}/>
+         <Buttons handle={handle}/>
+    </div>
   );
 }
+
 
 export default App;
